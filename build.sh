6ed63243d303c310e13c6fc93be06c749802d6f7
Devine Lu Linvega <aliceffekt@gmail.com>
2023-07-23 20:25:14 -0700

    Fixed issue with tile clearing

--- build.sh
+++ build.sh
@@ -1,43 +1,37 @@
 #!/bin/sh -e
 
+ID="noodle"
 ASM="uxncli $HOME/roms/drifblim.rom"
 EMU="uxnemu"
 LIN="uxncli $HOME/roms/uxnlin.rom"
 APP="$HOME/bin/butler push"
-
-SRC="src/noodle.tal"
-DST="bin/noodle.rom"
+SRC="src/${ID}.tal"
+DST="bin/${ID}.rom"
 
 CPY="$HOME/roms"
 ARG="lemon15x12.icn"
 
 APPID="hundredrabbits/noodle:uxn"
 
-echo ">> Cleaning"
 rm -rf bin
 mkdir bin
 
 if [[ "$*" == *"--lint"* ]]
 then
-    echo ">> Linting $SRC"
 	$LIN $SRC
 fi
 
-echo ">> Assembling $SRC"
 $ASM $SRC $DST
 
 if [[ "$*" == *"--save"* ]]
 then
-    echo ">> Saving $DST"
 	cp $DST $CPY
 fi
 
 if [[ "$*" == *"--push"* ]]
 then
-    echo ">> Pushing $DST"
 	$APP $DST $APPID
 fi
 
-echo ">> Running $DST"
 $EMU $DST $ARG
 
